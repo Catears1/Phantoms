e5d709c9ec2e59c86ab14ccec76c86e39b004224
TahleiRichardson <tahleirich@gmail.com>
2024-10-04 01:45:32 -0400

    Movement

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let img;
+var imgX = 200;
+var imgY = 200;
 
 function preload() {
   img = createImg('download.jpg');
@@ -11,12 +13,19 @@ function setup() {
 function draw() {
   //background(222);
   sketch2();
-  img.position(200, 200);
+  img.position(imgX, imgY);
   img.size(200,200);  
-
   
+  if (keyIsPressed) {
+   if (keyCode == RIGHT_ARROW) {
+    imgX +=5; 
+   }  else if (keyCode == LEFT_ARROW) {
+     imgX -= 5;
+   } else if (keyCode == UP_ARROW) {
+     imgY -= 5; 
+   } else if (keyCode == DOWN_ARROW) {
+     imgY +=5; 
+   }
+  }  
 
-  
 }
-
-
